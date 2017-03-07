33a034f2a9ea7fb2244586b39da2f5bdb1286ab0
Alex Ash <alex.ash112@gmail.com>
2017-03-07 16:46:35 -0500

    Updates to the main page

--- index.php
+++ index.php
@@ -1,29 +1,85 @@
-<link rel="stylesheet" type="text/css" href="./node_modules/semantic-ui/dist/semantic.min.css">
+<head>
+  <title>Alex Ash</title>
+  <link rel="shortcut icon" type="image/x-icon" href="images/my-logo.png" />
+  <link rel="stylesheet" type="text/css" href="./node_modules/semantic-ui/dist/semantic.min.css">
 <script
   src="https://code.jquery.com/jquery-3.1.1.min.js"
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
   crossorigin="anonymous"></script>
 <script src="node_modules/semantic-ui/dist/semantic.min.js"></script>
-<h1>Hello</h1>
-<div class="ui loading search">
-  <div class="ui icon input">
-    <input class="prompt" type="text" placeholder="Search...">
-    <i class="search icon"></i>
+</head>
+<div class="ui black inverted vertical header">
+    <div class="ui inverted segment">
+      <div class="ui inverted secondary pointing menu">
+        <a class="item">
+          <img src="images/my-logo.png">
+        </a>
+        <a class="active item">
+          Home
+        </a>
+        <a class="item">
+          Projects
+        </a>
+        <a class="item" href="contact">
+          Contact
+        </a>
+      </div>
+    </div>
+</div>
+
+<div class="ui vertical stripe segment">
+  <div class="ui middle centered aligned stackable grid container">
+    <div class="row">
+      <div class="eight wide column">
+        <h1 class="ui header centered">Welcome!</h1>
+        <h3 class="ui header">Aspiring Web Developer</h3>
+        <p>Hi! My name is Alex Ash and I am a student at St. Lawrence College in Kingston, Ontario.  I am taking the Computer Programmer Analyst program.  This site is designed to show you some of the projects I have worked on in the past and currently.  Feel free to contact me through any of the ways shown on my contact page if you have any questions or comments.</p>
+      </div>
+      <!-- <div class="six wide right floated column">
+        <img src="images/profile.jpg" class="ui large bordered rounded image" style="-webkit-transform: rotate(-90deg);
+    -moz-transform: rotate(-90deg);
+    -o-transform: rotate(-90deg);
+    -ms-transform: rotate(-90deg);
+    transform: rotate(-90deg);">
+      </div> -->
+    </div>
+    <div class="row">
+      <div class="center aligned column">
+        <a class="ui huge button">My Projects</a>
+      </div>
+    </div>
   </div>
-  <div class="results"></div>
 </div>
-<div class="ui secondary vertical pointing menu">
-  <a class="active item">
-    Home
-  </a>
-  <a class="item">
-    Messages
-  </a>
-  <a class="item">
-    Friends
-  </a>
+
+
+
+<div class="ui black inverted vertical footer segment">
+  <div class="ui container">
+    <div class="ui stackable inverted divided equal height stackable grid">
+        <div class="three wide column">
+          <h4 class="ui inverted header">About</h4>
+          <div class="ui inverted link list">
+            <a href="#" class="item">Home</a>
+            <a href="#" class="item">Projects</a>
+            <a href="#" class="item">Contact Information</a>
+          </div>
+        </div>
+        <div class="three wide column">
+          <h4 class="ui inverted header">Contact Info</h4>
+          <div class="ui inverted link list">
+            <a class="item" href="http://www.facebook.com" target="_blank"><i class="facebook square icon"></i> Facebook</a>
+            <a href="#" class="item"><i class="github square icon"></i> Github</a>
+            <a href="#" class="item"><i class="instagram icon"></i> Instagram</a>
+            <a href="#" class="item"><i class="linkedin icon"></i> Linked In</a>
+          </div>
+        </div>
+        <div class="seven wide column">
+          <h4 class="ui inverted header">Footer Header</h4>
+          <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+        </div>
+      </div>
+      <div class="ui horizontal inverted small divided link list">
+      <div class="centered">Copyright Alex Ash 2017</div>
+    </div>
+  </div>
 </div>
-<h2 class="ui center aligned icon header">
-  <i class="circular users icon"></i>
-  Alex Ash
-</h2>
