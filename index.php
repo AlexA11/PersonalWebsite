5bb260d93f8ec73e1ac625f3d40058541833f256
Alex Ash <alex.ash112@gmail.com>
2017-03-06 20:46:20 -0500

    More setup.  Testing pages

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
+<link rel="stylesheet" type="text/css" href="./node_modules/semantic-ui/dist/semantic.min.css">
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+<script src="node_modules/semantic-ui/dist/semantic.min.js"></script>
 <h1>Hello</h1>
+<div class="ui loading search">
+  <div class="ui icon input">
+    <input class="prompt" type="text" placeholder="Search...">
+    <i class="search icon"></i>
+  </div>
+  <div class="results"></div>
+</div>
+<div class="ui secondary vertical pointing menu">
+  <a class="active item">
+    Home
+  </a>
+  <a class="item">
+    Messages
+  </a>
+  <a class="item">
+    Friends
+  </a>
+</div>
+<h2 class="ui center aligned icon header">
+  <i class="circular users icon"></i>
+  Alex Ash
+</h2>
